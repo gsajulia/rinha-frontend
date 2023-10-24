23abda1e875e4150d8375ec157d98436c111be45
Julia Santi <juliagabriela98@gmail.com>
2023-10-23 23:20:21 -0300

    fix: types

--- src/models/components.ts
+++ src/models/components.ts
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 import { TJsonData } from "./structures";
 
 export type TFileLoader = {
-  setData: Dispatch<SetStateAction<TJsonData>>;
+  setData: Dispatch<SetStateAction<TJsonData | null>>;
 };
 
 export type TViewerTree = TJsonData;
